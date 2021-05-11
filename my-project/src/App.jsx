d5dfddd8a8b1326ec75ec221a7d148a155d4e56c
Pierre Legeay <l.pierre14@yahoo.fr>
2021-05-11 11:26:48 +0200

    remonter du travail

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 import React from 'react';
+import Defi from './components/Defi.jsx';
+
 
 function App() {
   return (
+    <div>
+      <Defi />
+    </div>
     
   );
 }
---
+++ my-project/src/components/Defi.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function Defi () {
+
+    return(
+        <div>
+            <h1>Bonjour</h1>
+            <form>
+                <input type="texte" placeholder="Votre nom ?" />
+                <input type="texte" placeholder="Soumettre votre défi!" />
+                <button>Confirmer</button>
+            </form>
+        </div>
+    )
+    
+}

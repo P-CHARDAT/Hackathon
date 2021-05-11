320a2d97d82ace3bd74135203a97d001ca5fd17c
Pierre Legeay <l.pierre14@yahoo.fr>
2021-05-11 16:18:56 +0200

    Le modal ok manque un peu de css

--- my-project/src/components/Accueil/Accueil.jsx
+++ my-project/src/components/Accueil/Accueil.jsx
@@ -1,15 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import useModal from "../Modal/useModal.jsx";
 import Modal from "../Modal/Modal";
+import "./Accueil.css";
 
 export default function Accueil() {
   const [isShowing, toggle] = useModal();
+  const [messages, setMessages] = useState([[]]);
+  const [userInput, setUserInput] = useState("");
+  const [userInpute, setUserInpute] = useState("");
+  const [userInputs, setUserInputs] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserInput("");
+    alert(
+      "The state is going to be updated, the User interface will be updated"
+    );
+    setMessages([...messages, [userInput, userInpute, userInputs]]);
+  };
 
   return (
     <div>
       <h1>salut</h1>
       <button onClick={toggle}>X</button>
-      <Modal isShowing={isShowing} hide={toggle} />
+      <Modal
+        isShowing={isShowing}
+        hide={toggle}
+        handleSubmit={handleSubmit}
+        setUserInput={setUserInput}
+        messages={messages}
+        setUserInputs={setUserInputs}
+        setUserInpute={setUserInpute}
+      />
+      <div>
+        {messages.map((groupeMessage, index) => (
+          <ul key={index} className={index % 2 === 0 ? "pair" : "impair"}>
+            {groupeMessage.map((message, i) => (
+              <li key={i} className="messages">
+                {message}
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
